d52238af86ea36c79de23cbd2216fee28d028dc4
Travis <46567145+TravisRoad@users.noreply.github.com>
2023-05-24 18:05:40 +0800

    trivial things

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ["lain.bgm.tv"],
+    // remotePatterns: [
+    //   {
+    //     protocol: "https",
+    //     hostname: "lain.bgm.tv",
+    //     port: "",
+    //     pathname: "/pic/cover/**",
+    //   },
+    // ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
