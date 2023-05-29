87f92c7d4a489bfe263ff3778ec5491d225b52b4
Travis <46567145+TravisRoad@users.noreply.github.com>
2023-05-29 23:38:10 +0800

    全局存储

--- src/app/lib/store.ts
+++ src/app/lib/store.ts
@@ -1,16 +1,23 @@
 import { item } from "@/bangumi-api/type";
+import { feed, feedCache } from "@/types/interface";
 import React from "react";
 
 interface StoreType {
   items: item[];
   addItem: (item: item) => void;
   removeItem: (item: item) => void;
+  feedCache: feedCache;
+  addFeedCache: (id: number, feed: feed, keyword: string) => void;
+  removeFeedCache: (id: number) => void;
 }
 
 const StoreContext = React.createContext<StoreType>({
   items: [],
   addItem: (item: item) => {},
   removeItem: (item: item) => {},
+  feedCache: {},
+  addFeedCache: (id: number, feed: feed, keyword: string) => {},
+  removeFeedCache: (id: number) => {},
 });
 
 export default StoreContext;
