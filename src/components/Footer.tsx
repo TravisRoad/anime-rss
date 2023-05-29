7c25912bdf4e9b14b0ca0d3ace20131f0305b1ef
Travis <46567145+TravisRoad@users.noreply.github.com>
2023-05-29 23:50:33 +0800

    add footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,27 @@
+import Link from "next/link";
 import React from "react";
 
 export default function Footer() {
-  return <footer></footer>;
+  return (
+    <footer className="flex flex-col items-center justify-center tablet:max-w-4xl mx-auto mt-4 text-sm mb-8">
+      <div>
+        Created by{" "}
+        <Link
+          href="https://blog.lxythan2lxy.cn/about"
+          className="underline text-nord-10"
+        >
+          Travis
+        </Link>
+      </div>
+      <div>
+        <Link
+          href="https://github.com/TravisRoad/anime-rss"
+          className="underline text-nord-10"
+        >
+          anime-rss
+        </Link>
+      </div>
+      <div className="sr-only">一直看番一直爽</div>
+    </footer>
+  );
 }
